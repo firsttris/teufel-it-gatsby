2eacbcea9f17c8896982d9a6349f1fa6ca341223
firsttris <tristanteufel@googlemail.com>
2018-03-21 20:15:27 +0100

    fix(config): add prefixPath

---
+++ src/components/404.tsx
@@ -0,0 +1,53 @@
+import Link from 'gatsby-link';
+import * as React from 'react';
+import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl';
+
+const Lines = require('../assets/images/backgrounds/lines.png');
+const Unicorn = require('../assets/images/NotFound/unicorn-small.png');
+
+const containerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  fontFamily: 'monospace',
+  backgroundImage: `url(${Lines})`,
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  paddingTop: '100px'
+};
+const imageStyle: React.CSSProperties = { marginTop: '50px', marginBottom: '50px', height: '200px' };
+const spacerStyle: React.CSSProperties = { height: '80px' };
+
+const NotFound = (props: InjectedIntlProps) => (
+  <div style={containerStyle}>
+    <img src={Unicorn} alt="Not Found" style={imageStyle} />
+    <h2>
+      <FormattedMessage id="ERROR_TEXT" />
+    </h2>
+    <div>
+      <FormattedMessage id="ERROR_NOT_FOUND" />
+    </div>
+    <div>
+      <FormattedMessage id="ERROR_CONTACT_US" />
+    </div>
+    <div>
+      <div className="spacer" style={spacerStyle} />
+      <div>
+        <b>
+          <FormattedMessage id="CONTACT_TITLE" />
+        </b>
+      </div>
+      <div>
+        <a href="https://www.teufel-it.de">www.teufel-it.de</a>
+      </div>
+      <div>
+        <a href="mailto:info@teufel-it.de">info@teufel-it.de</a>
+      </div>
+      <br />
+      <Link to={`/${props.intl.locale}/`} className="btn btn-dark">
+        <FormattedMessage id="ERROR_START" />
+      </Link>
+    </div>
+  </div>
+);
+
+export default injectIntl<{}>(NotFound);
---
+++ src/components/Github.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import Contact from './Contact';
+import FullImage from './FullImage';
+import GithubItem from './GithubItem';
+import Header from './Header';
+import Impressum from './Impressum';
+import SectionHeaderWithSubRow from './SectionHeaderWithSubRow';
+
+const Lines = require('./../assets/images/backgrounds/lines.png');
+const OldComputer = require('./../assets/images/code.jpg');
+
+const containerStyle: React.CSSProperties = { backgroundImage: `url(${Lines})` };
+
+interface Props {}
+
+interface State {
+  searchInput: string;
+  filteredRepositories: any[];
+  repositories: any[];
+}
+
+class Github extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+      filteredRepositories: [],
+      repositories: []
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.callService(), 1000);
+  }
+
+  callService() {
+    fetch('https://api.github.com/users/firsttris/repos').then(data =>
+      data.json().then(result => {
+        const repos = result.filter((repository: any) => repository.fork === false);
+        repos.sort((a: any, b: any) => b.stargazers_count - a.stargazers_count);
+        this.setState({ repositories: repos, filteredRepositories: repos });
+      })
+    );
+  }
+
+  onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      document.activeElement.blur();
+    }
+  };
+
+  search = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const repos = this.state.repositories.filter(repository =>
+      repository.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    this.setState({ filteredRepositories: repos, searchInput: event.target.value });
+  };
+
+  reset = () => {
+    this.setState({ searchInput: '', filteredRepositories: this.state.repositories });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid" style={containerStyle}>
+        <div className="text-center">
+          <FullImage image={OldComputer} height="550px" />
+          <Header />
+        </div>
+        <div className="container">
+          <SectionHeaderWithSubRow title="NAVIGATION_PROJECTS" text="GITHUB_SUBTITLE" />
+          <div className="form-group pt-3">
+            <div className="input-group">
+              <input
+                type="search"
+                className="form-control"
+                id="Search"
+                placeholder="Search"
+                value={this.state.searchInput}
+                onChange={this.search}
+                onKeyPress={this.onEnter}
+              />
+              <div className="input-group-prepend" onClick={this.reset} style={{ cursor: 'pointer' }}>
+                <div className="input-group-text">
+                  <i className="fas fa-trash-alt" aria-hidden="true" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="list-group pb-3">
+            {this.state.filteredRepositories.map((repository, index) => <GithubItem key={index} repo={repository} />)}
+          </div>
+        </div>
+        <Contact />
+        <Impressum />
+      </div>
+    );
+  }
+}
+
+export default Github;

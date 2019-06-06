b75bfc8c6937de4a7b7ec8bd494d6ea699867766
firsttris <tristanteufel@googlemail.com>
2019-06-06 14:09:04 +0200

    feat(htaccess):

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ const path = require(`path`);
 module.exports = {
   pathPrefix: '/teufel-it-gatsby',
   siteMetadata: {
-    title: 'teufel-it'
+    title: 'teufel-it',
+    siteUrl: `https://teufel-it.de`
   },
   plugins: [
     'gatsby-plugin-typescript',
@@ -23,6 +24,41 @@ module.exports = {
         trackingId: 'UA-70468785-1',
         sampleRate: 5,
         siteSpeedSampleRate: 10
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Teufel IT, Software ist unser Handwerk`,
+        short_name: `Teufel IT`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `ffffff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        exclude: [],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+  
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`
       }
     }
   ]
